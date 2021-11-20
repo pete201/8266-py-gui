2410c96b889d0936f4476251e6fd8718b0264d1a
pete201 <peterdeeming@hotmail.com>
2021-11-20 21:59:52 +0000

    displays values to 2 decimal places

--- display window.py
+++ display window.py
@@ -1,5 +1,6 @@
 from tkinter.constants import RIGHT
 import PySimpleGUI as sg
+from decimal import Decimal
 
 layout = [
     [sg.Text('Yaw:', size=8), sg.Text('value', key='yawOut')],
@@ -13,17 +14,15 @@ layout = [
 window = sg.Window('8266 readout', layout)
 
 
-
 while True:
     event, values = window.read()
 
-    if event in ('Exit', None): break
+    if event in ('Exit', None): break                                   # event 'None' is passed when closing the window by clicking x
 
-    #print(values['yawIn'])
-
-    window['yawOut'].update(values['yawIn'])
-    window['pitchOut'].update(values['pitchIn'])
-    window['rollOut'].update(values['rollIn'])
-    
+    try:                                                                # try necessary to handle non-decimal values (e.g. blank value)
+        window['yawOut'].update(round(Decimal(values['yawIn']), 2))     # 'values' are strings - convert to Decimal and round to 2 decimal places
+        window['pitchOut'].update(round(Decimal(values['pitchIn']),2))
+        window['rollOut'].update(round(Decimal(values['rollIn']),2))
+    except: pass
 
 window.close()
